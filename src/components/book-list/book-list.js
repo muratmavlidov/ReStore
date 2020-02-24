89f7b42fed4f0b915049fd5eda6d7baafc910edc
Murat Mavlidov <mmavlidov96@mail.ru>
2020-02-24 23:31:25 +0300

    refactor store and reducer, added applyMiddleware

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -49,7 +49,7 @@ class BookListContainer extends Component {
   }
 }
 
-const mapStateToProps = ({ books, loading, error }) => {
+const mapStateToProps = ({ bookList: { books, loading, error } }) => {
   return { books, loading, error }
 };
 
--- src/store.js
+++ src/store.js
@@ -1,7 +1,15 @@
-import { createStore } from 'redux';
-
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const stringMiddleWare = () => (next) => (action) => {
+  if (typeof action === 'string') {
+    return next({
+      type: action
+    });
+  }
+  return next(action);
+}
+
+const store = createStore(reducer, applyMiddleware(stringMiddleWare));
 
 export default store;
